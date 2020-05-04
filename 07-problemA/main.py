6aa396d020590bbb5c9d39c89b32695f47aa99e3
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-05-04 11:26:07 +0200

    :construction: starting the problem A of 2007

---
+++ 07-problemA/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author: Baptiste Bertrand-Rapello
+# @Date:   2020-05-04 10:45:25
+# @Last Modified by:   Baptiste Bertrand-Rapello
+# @Last Modified time: 2020-05-04 11:25:36
+
+import sys
+
+def getAllBlood(inputBlood):
+	parentOneBlood = ""
+	parentOneRhesus = ""
+	parentTwoBlood = ""
+	parentTwoRhesus = ""
+	childBlood = ""
+	listBlood = inputBlood.split(" ")
+	
+
+def main():
+	allInput = []
+	print("here is where the magik happen")
+	for line in sys.stdin:
+		print("en premier: ", line)
+		allInput.append(line)
+
+if __name__=="__main__":
+	main() 
