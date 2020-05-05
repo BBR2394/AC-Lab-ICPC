22eeb5cca69120141dfe68fe7103d25f067e9974
Antoine <aberthier@capensis.fr>
2020-05-05 12:58:42 +0200

    🚧 WIP

--- 05-problemeC/src/Juge_gps.py
+++ 05-problemeC/src/Juge_gps.py
@@ -6,6 +6,10 @@ class map() :
     def __init__(self, nbVille, objectif):
         self.viles = [ville(x) for x in range(nbVille)]
         self.objectif = self.viles[objectif]
+        self.routes = {}
+
+    def creation_rout(self, a, b, longeur):
+            pass
 
 
 
