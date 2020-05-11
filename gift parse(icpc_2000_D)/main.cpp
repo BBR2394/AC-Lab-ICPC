d2a00a9a8973c9af90d9179f63b1c538c3296563
Tchakpele <k.tchakpele@gmail.com>
2020-05-11 14:30:42 +0200

    update

--- gift parse(icpc_2000_D)/main.cpp
+++ gift parse(icpc_2000_D)/main.cpp
@@ -30,7 +30,7 @@ cout << "Impossible d'ouvrir le fichier " << endl;
 }
 else{
 
-fscanf(f, "%f %f %f %f %f %f %f", &input[0], &input[1], &input[2],&input[3],&input[4],&input[5],&input[6]);
+fscanf(f, "%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f  ", &input[0], &input[1], &input[2],&input[3],&input[4],&input[5],&input[6],&input[7],&input[8],&input[9],&input[10],&input[11],&input[12],&input[13],&input[14],&input[15]);
 
 if(input[0]==3){
 
@@ -68,13 +68,18 @@ if(input[0]==3){
   largeur = aire_gift*2/longueur;
 
 aire_minimal= ceil(longueur * largeur) ;
+aire_maximal= ceil((longueur*longueur/2)) ;
 
 
 
 cout << "Gift1 : " << endl;
-cout << "Min area : " <<aire_minimal*1000<< endl;
+cout << "Min_area : " <<aire_minimal*1000<< endl;
 
-}else
+cout << "Max_area : " <<aire_maximal*1000<< endl;
+
+cout<<" " <<endl;
+
+}
 if(input[7]==4){
 
     x1=input[8];
@@ -87,6 +92,21 @@ if(input[7]==4){
    y4=input[15];
 
 
+   dist1=sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+  dist2=sqrt((x2-x3)*(x2-x3) + (y2-y3)*(y2-y3));
+  dist3=sqrt((x1-x4)*(x1-x4) + (y1-y4)*(y1-y4));
+  dist4=sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
+
+  longueur=max(max(dist1,dist2),max(dist3,dist4));
+  largeur=min(min(dist1,dist2),min(dist3,dist4));
+
+  aire_minimal=longueur*largeur;
+  aire_maximal=aire_minimal*2;
+
+  cout << "Gift2 : " << endl;
+cout << "Min_area : " <<aire_minimal*1000<< endl;
+
+cout << "Max_area : " <<aire_maximal*1000<< endl;
 
 }
 
