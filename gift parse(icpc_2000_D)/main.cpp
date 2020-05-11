f2263139fb7cd74ed4e59ee7368f006c98424765
Tchakpele <k.tchakpele@gmail.com>
2020-05-11 12:59:13 +0200

    update

--- gift parse(icpc_2000_D)/main.cpp
+++ gift parse(icpc_2000_D)/main.cpp
@@ -8,17 +8,19 @@ using namespace std;
 int main()
 {
     FILE *f = NULL;
-    char input [100]={0};
-    int value;
-    int a=0;
+    float input [100]={0};
     float aire_minimal ;
     float aire_maximal;
-    int x1,y1,x2,y2,x3,y3,y4,x4;
+    float x1,y1,x2,y2,x3,y3,y4,x4;
     float dist1,dist2,dist3,dist4;
-    int longueur,largeur;
-    int semi_perimetre;
+    float longueur,largeur;
+    float semi_perimetre;
     float aire_gift;
     f=fopen("test.txt","r");
+
+
+
+
 
     if(f==NULL){
 
@@ -28,15 +30,8 @@ cout << "Impossible d'ouvrir le fichier " << endl;
 }
 else{
 
-while(value !=EOF){
+fscanf(f, "%f %f %f %f %f %f %f", &input[0], &input[1], &input[2],&input[3],&input[4],&input[5],&input[6]);
 
-value = fgetc(f);
- input[a]=value;
-
- a++;
-
-}
-cout << input[3] << endl;
 if(input[0]==3){
 
    x1=input[1];
@@ -46,9 +41,14 @@ if(input[0]==3){
    x3=input[5];
    y3=input[6];
 
-  dist1=sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+
+
+
+  dist1=sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
   dist2=sqrt((x2-x3)*(x2-x3) + (y2-y3)*(y2-y3));
   dist3=sqrt((x1-x3)*(x1-x3) + (y1-y3)*(y1-y3));
+
+
 
   if(dist1>dist2 && dist1>dist3){
 
@@ -61,18 +61,18 @@ if(input[0]==3){
     longueur=dist3;
   }
 
-  semi_perimetre=(dist1+dist2+dist3)/2;
 
+  semi_perimetre=(dist1+dist2+dist3)/2;
   aire_gift=sqrt(semi_perimetre*(semi_perimetre-dist1)*(semi_perimetre-dist2)*(semi_perimetre-dist3));
 
   largeur = aire_gift*2/longueur;
 
-aire_minimal= longueur * largeur ;
+aire_minimal= ceil(longueur * largeur) ;
 
 
 
 cout << "Gift1 : " << endl;
-cout << "Min area : " <<aire_minimal<< endl;
+cout << "Min area : " <<aire_minimal*1000<< endl;
 
 }else
 if(input[7]==4){
